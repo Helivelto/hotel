c72190ccb48934782a7ca0a1c437f69f3072ddaf
Helivelto <helivelto.souza321@gmail.com>
2021-10-03 17:38:05 -0300

    finishing a login page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const faceBox = document.querySelector('#facebox')
+const googleBox = document.querySelector('#googlebox')
+// const faceBox_login = document.querySelector('#facebox_login')
+// const googleBox_login = document.querySelector('#googlebox_login')
+
+faceBox.addEventListener('click', function erro_login(){
+    alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+})
+googleBox.addEventListener('click', function erro_login(){
+    alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+})
+// faceBox_login.addEventListener('click', function erro_login(){
+//     alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+// })
+// googleBox_login.addEventListener('click', function erro_login(){
+//     alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+// })
