f8982362922f1a81a84e6742befff7d73a9240ae
Helivelto <helivelto.souza321@gmail.com>
2021-10-26 15:24:07 -0300

    formating document

---
+++ js/script_cad.js
@@ -0,0 +1,9 @@
+const faceBox = document.querySelector('#facebox')
+const googleBox = document.querySelector('#googlebox')
+
+faceBox.addEventListener('click', function erro_login(){
+    alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+})
+googleBox.addEventListener('click', function erro_login(){
+    alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+})
---
+++ js/script_login.js
@@ -0,0 +1,9 @@
+const faceBox_login = document.querySelector('#facebox_login')
+const googleBox_login = document.querySelector('#googlebox_login')
+
+faceBox_login.addEventListener('click', function erro_login(){
+    alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+})
+googleBox_login.addEventListener('click', function erro_login(){
+    alert('[ERRO] Essa funcionalidade ainda não está disponível.')
+})
